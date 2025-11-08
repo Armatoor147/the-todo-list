a5f742d02e051acd3cbedea2f7a32b433d31f58c
Vincent TOOR AZORIN <vincent.toor@gmail.com>
2025-11-08 21:31:38 +0000

    Set up Express, CORS, MongoDB, APIs

---
+++ src/backend/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const tasksRouter = require('./routes/tasks');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI
+
+const corsOptions = {
+  origin: 'http://the-todo-list:3000', // TODO: Replace this with frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
+mongoose.connect(MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use(bodyParser.json());
+app.use('/api/tasks', tasksRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
